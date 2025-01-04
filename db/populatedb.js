9700416902b5f9c9cafafb560d1ab71211341957
BalazsBanfi <banfibalazs81@gmail.com>
2025-01-04 13:27:27 +0100

    Populate db

--- db/populatedb.js
+++ db/populatedb.js
@@ -4,7 +4,7 @@ const { Client } = require("pg");
 require("dotenv").config();
 
 const CATEGORIES_SQL = `
-DROP TABLE IF EXIST categories;
+DROP TABLE IF EXISTS categories;
 
 CREATE TABLE IF NOT EXISTS categories (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,  
@@ -21,26 +21,22 @@ const SHOES_SQL = `
 DROP TABLE IF EXISTS shoes;
 
 CREATE TABLE IF NOT EXISTS shoes (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,  
   category_id INTEGER,
   name VARCHAR ( 64 ),
   brand VARCHAR ( 64 ),
   color VARCHAR ( 16 ),
   size INTEGER,
   price DECIMAL (4, 2),
-  shoetype INTEGER,
   added DATE
 );
 
-INSERT INTO shoes (category_id INTEGER,
-  name,
-  brand,
-  color,
-  size,
-  price,
-  shoetype,
-  added) 
-VALUES ('Racing'), ('Daily trainer'), ('Trail'), ('Stability'), ('Value'), ('Other');
-
+INSERT INTO shoes
+  (category_id, name, brand, color, size, price, added) 
+  VALUES (5, 'Run Active Grip', 'Kalenji', 'Blue', 46, 22.50, '2019-01-14'),
+  (2, 'Rouge Rabbit IV', 'Li-Ning', 'Black', 46, 24.20, '2023-12-27'),
+  (5, 'Vitaly', 'Joma', 'Black', 46, 26.70, '2024-08-10'),
+  (1, 'Red Hare 7', 'Li-Ning', 'Black', 46, 28.00, '2024-10-12');
 `;
 
 const initDb = async () => {
@@ -50,8 +46,8 @@ const initDb = async () => {
     port: process.env.PGPORT,
     user: process.env.PGUSER,
     password: process.env.PGPASSWORD,
-    database: process.env.PGDATABASE,
-    ssl: true
+    database: process.env.PGDATABASE
+    //,ssl: true
   });
   await client.connect();
   await client.query(CATEGORIES_SQL);
